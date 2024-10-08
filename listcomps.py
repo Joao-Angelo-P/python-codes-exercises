3d499b23c7b8e1362558c150b7498e6e474ae2c4
Joao-Angelo-P <78273535+Joao-Angelo-P@users.noreply.github.com>
2024-10-08 19:30:10 -0400

    Some interesting code with the list comprehension concept

---
+++ listcomps.py
@@ -0,0 +1,17 @@
+# treinando "List Comprehension" ou listcomps
+# quebra de linhas são ignoradas dentro de {}, [], ()
+
+a = [
+	x + 1
+	for x in range(10)
+] # [1, 2, 3, 4, 5, 6, , 7, 8, 9, 10]
+
+b = {
+	x + 1
+	for x in range(10)
+} # {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+c = (
+	x + 1
+	for x in range(10)
+) # precisa iterar o objeto. loop for: for i in c: print(i)
