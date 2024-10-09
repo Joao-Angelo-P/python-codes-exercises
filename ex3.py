f71c2803348f74cb89b58aa727b88214842e3760
Joao-Angelo-P <78273535+Joao-Angelo-P@users.noreply.github.com>
2024-10-09 10:20:49 -0400

    Demostrate the spaces and break lines are ignored by the brackets

--- ex3.py
+++ ex3.py
@@ -3,4 +3,8 @@ from pathlib import Path
 
 def __load__file(filepath: Path | str) -> list[str]:
 	with open(filepath, 'r') as f:
-		return [x.strip() for x in f if x != '/n']
+		return [
+		x.strip()
+		for x in f
+		if x != '/n'
+]
