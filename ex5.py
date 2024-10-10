532b150908a394e0d5694b4bb44ec2151a9fddbe
Joao-Angelo-P <78273535+Joao-Angelo-P@users.noreply.github.com>
2024-10-10 10:43:31 -0400

    Listcomps vs map and filter example

---
+++ ex5.py
@@ -0,0 +1,5 @@
+# Listcomps vs map and filter
+symbols = "%$¨&*@"
+__ascii = [ord(x) for x in symbols if ord(x) > 41] # listcomps
+
+__ascii_ = list(filter(lambda x: x > 41, map(ord, symbols))) # map and filter
