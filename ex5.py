11774aab18cde8a22e61c058d37896ed89e9cb1c
Joao-Angelo-P <78273535+Joao-Angelo-P@users.noreply.github.com>
2024-11-29 20:13:00 -0400

    ___

--- ex5.py
+++ ex5.py
@@ -3,3 +3,6 @@ symbols = "%$¨&*@"
 __ascii = [ord(x) for x in symbols if ord(x) > 41] # listcomps
 
 __ascii_ = list(filter(lambda x: x > 41, map(ord, symbols))) # map and filter
+
+# Unpacking sequences
+
