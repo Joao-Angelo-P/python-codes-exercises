c02cc4f091071b2f31c1addad3b398a1aae15c0c
Joao-Angelo-P <78273535+Joao-Angelo-P@users.noreply.github.com>
2024-12-02 15:20:18 -0400

    Include hashbang and formatting the list

--- ex8.py
+++ ex8.py
@@ -1,10 +1,18 @@
+#!/usr/bin/python3
 # *args
 # uma função para elevar os numeros ao quadrado, mas se for float é para multiplicar por 100
 # depois elevar ao quadrado e dividir por 10
 # observação: estou contando floats com 1 casa decimal, ex: 1.1
 
 def f_2(*args):
-	l = [i**2 if type(i) is not float else ((i*10)**2)/10 for i in args]
+
+    l = [
+         i**2
+         if type(i) is not float
+         else pow(i, 2)
+         for i in args
+         ]
+
 	print(l)
 
 f_2() # executa o programa
