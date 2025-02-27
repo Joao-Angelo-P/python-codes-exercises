a978e6b47626e5e497e5363741bf1b8f04cf831b
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-02-26 22:20:44 -0400

    Update if_else_input_1_line.py

--- diversos/if_else_input_1_line.py
+++ diversos/if_else_input_1_line.py
@@ -1,3 +1,4 @@
 print(f'{nome} | {sobrenome}') if (bool(nome:=input()), bool(sobrenome:=input())) == (True, True) else print('Digitou nada') # uma linha para receber input criar um bloco de condicional e imprimir
 print('teste') if False else [[1 if not True else 2] for i in range(1) if True][0][0] # if_else aninhado e um if_else dentro de um listcomp
 [[1 if(result:=input()) else 2][0] for i in range(3)] # rapaz? 
+print('A palavra é palíndromo') if ((entrada:=input('Digite uma palavra:').replace(' ', '').lower())==entrada[::-1]) else print('A palavra não é palíndromo') # palíndromo em uma linha
