dfe4f18482b381df463cd27c3b860ef44e73931a
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-04-29 10:21:00 -0400

    Update fatorials.py
    Se você quiser uma validação do número antes de efetuar a fatoração há duas formas. Primeira é para usar validação na "fatorial_1", que é o comentário abaixo dela e a outra é colocando decorador "naturais" nas funções de 2 a 4(funções não anônimas).

--- fatorial/fatorials.py
+++ fatorial/fatorials.py
@@ -10,9 +10,20 @@ __all__=['fatorial_1', #1000 loops, best of 5: 62.8 usec per loop
          'fatorial_4'  #1000 loops, best of 5: 63.2 usec per loop
          ]
 
+def naturais(func):
+    @wraps(func)
+    def wrapper(*args, **kw):
+        for i in args:
+            if not (i > 0 and type(i) is int):
+                raise ValueError("Não é possivel fatoração com \"%s\"" % i)
+        r = func(*args, **kw)
+        return r
+    return wrapper
 
 fatorial_1 = lambda n: reduce(mul, range(1, n+1, 1), 1)
+#opcional:fatorial_1 = lambda n, func=fatorial_1: func(n) if n > 0 and type(n) is int else print('Operacao invalida')
 
+#opcional: @naturais
 def fatorial_2(n):
     if not n:
         return 1
