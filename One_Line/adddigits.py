64b13010423e388b8c21cfff5eea34b0707cd071
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-05-10 22:09:42 -0400

    Update adddigits.py
    Without class declaration.

--- One_Line/adddigits.py
+++ One_Line/adddigits.py
@@ -1,2 +1,3 @@
 #258. Add Digits
 class Solution: addDigits=lambda self, num: self.addDigits(sum(divmod(num, 10))) if num >= 10 else num
+Solution = type("Solution", (), dict(addDigits=lambda self, num: self.addDigits(num%10+num//10) if num >= 10 else num))
