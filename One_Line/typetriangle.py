097c598676db31c4bc77686db04dc3b5d4f6f017
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-05-19 11:51:27 -0400

    Update typetriangle.py
    trick to "set(nums)"

--- One_Line/typetriangle.py
+++ One_Line/typetriangle.py
@@ -1,2 +1,2 @@
 #3024. Type of Triangle
-class Solution: triangleType= lambda self, nums: "none" if sum(sorted(nums)[:2]) <= sorted(nums)[-1] else {1:"equilateral", 2:"isosceles", 3:"scalene"}[len(set(nums))]
+class Solution: triangleType= lambda self, nums: "none" if sum(sorted(nums)[:2]) <= sorted(nums)[-1] else {1:"equilateral", 2:"isosceles", 3:"scalene"}[len({*nums})]
