eaaaacb2cab55c1f5161e94464458a2be866dd2f
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-05-19 12:08:25 -0400

    Update typetriangle.py
    trick with duck typing

--- One_Line/typetriangle.py
+++ One_Line/typetriangle.py
@@ -1,2 +1,3 @@
 #3024. Type of Triangle
 class Solution: triangleType= lambda self, nums: "none" if sum(sorted(nums)[:2]) <= sorted(nums)[-1] else {1:"equilateral", 2:"isosceles", 3:"scalene"}[len({*nums})]
+class Solution: triangleType= lambda self, nums: ["none", "equilateral", "isosceles", "scalene"][(sum(sorted(nums)[:2]) > sorted(nums)[-1]) * len({*nums})]
