c53028a285c93660c7916ab4ef24fcc9d7959b47
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-05-21 11:57:53 -0400

    Update custom_enumerate.py
    test with doc's module

--- custom_enumerate.py
+++ custom_enumerate.py
@@ -1,10 +1,19 @@
 """
 Work with any Iterable or Iterator
-"""
-from typing import Iterable, Tuple, Any, Generator
+>>> list(cenumerate('ABC'))
+[(0, 'A'), (1, 'B'), (2, 'C')]
+>>> list(cenumerate([0, 1, 'AB']))
+[(0, 0), (1, 1), (2, 'AB')]
+>>> list(cenumerate(range(1, 6, 1)))
+[(0, 1), (1, 2), (2, 3), (3, 4), (4, 5)]"""
 
+from typing import Iterable, Tuple, Any, Generator
 def cenumerate(iterable: Iterable) -> Generator[Tuple[int, Any], None, None]:
     i = 0
     for item in iterable:
         yield i, item
         i += 1
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=1)
