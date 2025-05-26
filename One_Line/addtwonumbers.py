df6c08a161209a54f19417483f0ba9d48a14241e
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-05-26 11:51:41 -0400

    Update addtwonumbers.py
    linter formatting

--- One_Line/addtwonumbers.py
+++ One_Line/addtwonumbers.py
@@ -1,3 +1,6 @@
-#2235-Add Two Integers
-class Solution: sum = lambda self, num1, num2: num1+num2
-Solution = type('Solution', (object,), {'sum': lambda self, num1, num2: num1+num2})
+# 2235-Add Two Integers
+class Solution:
+    sum = lambda self, num1, num2: num1 + num2
+
+
+Solution = type("Solution", (object,), {"sum": lambda self, num1, num2: num1 + num2})
