48b3501f5a19b9194d6d896ef253dec9fc85ae96
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-05-26 11:52:52 -0400

    Update fibonacci.py
    linter formatting

--- One_Line/fibonacci.py
+++ One_Line/fibonacci.py
@@ -1,2 +1,3 @@
-#509. Fibonacci Number
-class Solution: fib=lambda self, n: n if n<2 else self.fib(n-1)+self.fib(n-2)
+# 509. Fibonacci Number
+class Solution:
+    fib = lambda self, n: n if n < 2 else self.fib(n - 1) + self.fib(n - 2)
