5e9546f6980c7a11f9e34a51f50bd99b71042b89
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-05-26 11:50:33 -0400

    Update adddigits.py
    linter formatting

--- One_Line/adddigits.py
+++ One_Line/adddigits.py
@@ -1,3 +1,16 @@
-#258. Add Digits
-class Solution: addDigits=lambda self, num: self.addDigits(sum(divmod(num, 10))) if num >= 10 else num
-Solution = type("Solution", (), dict(addDigits=lambda self, num: self.addDigits(num%10+num//10) if num >= 10 else num))
+# 258. Add Digits
+class Solution:
+    addDigits = lambda self, num: (
+        self.addDigits(sum(divmod(num, 10))) if num >= 10 else num
+    )
+
+
+Solution = type(
+    "Solution",
+    (),
+    dict(
+        addDigits=lambda self, num: (
+            self.addDigits(num % 10 + num // 10) if num >= 10 else num
+        )
+    ),
+)
