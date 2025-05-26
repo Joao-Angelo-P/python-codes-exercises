8924cbd219bb740a8f307095f30556d535652a07
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-05-26 11:54:19 -0400

    Update typetriangle.py
    It is one line. Linter formatting

--- One_Line/typetriangle.py
+++ One_Line/typetriangle.py
@@ -1,4 +1,23 @@
-#3024. Type of Triangle
-class Solution: triangleType= lambda self, nums: "none" if sum(sorted(nums)[:2]) <= sorted(nums)[-1] else {1:"equilateral", 2:"isosceles", 3:"scalene"}[len({*nums})]
-class Solution: triangleType= lambda self, nums: ["none", "equilateral", "isosceles", "scalene"][(sum(sorted(nums)[:2]) > sorted(nums)[-1]) * len({*nums})]
-class Solution: triangleType= lambda self, nums: ["","none", "equilateral", "isosceles", "scalene"][pow(len({*nums})+1,sum(sorted(nums)[:2]) > sorted(nums)[2])]
+# 3024. Type of Triangle
+class Solution:
+    triangleType = lambda self, nums: (
+        "none"
+        if sum(sorted(nums)[:2]) <= sorted(nums)[-1]
+        else {1: "equilateral", 2: "isosceles", 3: "scalene"}[len({*nums})]
+    )
+
+
+class Solution:
+    triangleType = lambda self, nums: ["none", "equilateral", "isosceles", "scalene"][
+        (sum(sorted(nums)[:2]) > sorted(nums)[-1]) * len({*nums})
+    ]
+
+
+class Solution:
+    triangleType = lambda self, nums: [
+        "",
+        "none",
+        "equilateral",
+        "isosceles",
+        "scalene",
+    ][pow(len({*nums}) + 1, sum(sorted(nums)[:2]) > sorted(nums)[2])]
