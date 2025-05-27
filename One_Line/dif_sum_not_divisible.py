554a7b948d37c5b41ebfdca5e033e950582a601c
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-05-27 15:38:55 -0400

    Update dif_sum_not_divisible.py

--- One_Line/dif_sum_not_divisible.py
+++ One_Line/dif_sum_not_divisible.py
@@ -3,3 +3,10 @@ class Solution:
     differenceOfSums = lambda self, n, m: sum(
         i if i % m else -i for i in range(1, n + 1)
     )
+
+
+class Solution:
+    differenceOfSums = lambda self, n, m: sum(
+        [*range(1, n + 1), *[*range(-m, -n - 1, -m)] * 2]
+    )
+
