7a00a73843164cd6c358d2f660c1ccfc48109326
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-06-18 15:53:36 -0400

    Update code1.py
    walrus operator for less repeat struture.

--- One_Line/leetcode_2966/code1.py
+++ One_Line/leetcode_2966/code1.py
@@ -1,8 +1,10 @@
 """
-The decorator is for sorting list before insert it in divideArray function, but still one line solution. 
+The decorator is for sorting list before insert it in divideArray function, but still one line solution.
 Without the decorator, this program don't pass all the tests. It stuck in "Time Limit Exceeded" exception.
 *The 'code2.py' is the code without decorator*.
 """
+
+
 class Solution:
     def d_sort(f):
         def wrap(*args, **kw):
@@ -15,10 +17,13 @@ class Solution:
 
     # self, nums: List[int], k: int
     divideArray = lambda self, nums, k, l: (
-        [nums[i : i + 3] for i in range(0, l, 3) if nums[i + 2] - nums[i] <= k]
-        if len([nums[i : i + 3] for i in range(0, l, 3) if nums[i + 2] - nums[i] <= k])
-        * 3
-        == len(nums)
+        arr
+        if (
+            arr := [
+                nums[i : i + 3] for i in range(0, l, 3) if nums[i + 2] - nums[i] <= k
+            ]
+        )
+        and len(arr) * 3 == len(nums)
         else []
     )
 
