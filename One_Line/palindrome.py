df377cd6d7c68ec4662eda2a0dbbcdae8ac3bc3b
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-07-07 11:11:12 -0400

    Create palindrome.py

---
+++ One_Line/palindrome.py
@@ -0,0 +1,21 @@
+def filtrar(func):
+    def _wrap(*args):
+        if args[0].bool_:
+            if args[1] % 10 == 0 and args[1] or args[1] < 0:
+                return False
+            args[0].bool_ = False
+            args[0].num = args[1]
+            args[0].rev = 0
+        return func(*args)
+
+    return _wrap
+
+
+class Solution:
+    bool_ = True
+    isPalindrome = lambda self, x: (
+        self.isPalindrome(x // 10)
+        if x and setattr(self, "rev", self.rev * 10 + x % 10) is None
+        else self.num == self.rev
+    )
+    isPalindrome = filtrar(isPalindrome)
