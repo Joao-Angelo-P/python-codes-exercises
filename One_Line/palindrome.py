7aa01f8318f0907497a7095da72d7c7af6474a1c
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-07-07 15:51:03 -0400

    Update palindrome.py

--- One_Line/palindrome.py
+++ One_Line/palindrome.py
@@ -4,7 +4,7 @@ def filtrar(func):
             if args[1] % 10 == 0 and args[1] or args[1] < 0:
                 return False
             args[0].bool_ = False
-            args[0].num = args[1]
+            # args[0].num = args[1]
             args[0].rev = 0
         return func(*args)
 
@@ -16,6 +16,6 @@ class Solution:
     isPalindrome = lambda self, x: (
         self.isPalindrome(x // 10)
         if x and setattr(self, "rev", self.rev * 10 + x % 10) is None
-        else self.num == self.rev
+        else x in (self.rev, self.rev//10)
     )
     isPalindrome = filtrar(isPalindrome)
